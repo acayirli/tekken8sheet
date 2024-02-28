9681c60c585603a7ba0d4102cdaec59fd9449aff
Deniz Gunay <andacg1@hotmail.com>
2024-02-27 16:40:00 -0800

    feat: tekken-parser

--- src/types.ts
+++ src/types.ts
@@ -1,11 +1,13 @@
-export type InputsType = "1" | "2" | "3" | "4" | "1+2" | "1+3" | "1+4" | "2+3" | "2+4" | "3+4" | "1+2+3+4" 
+import type { TekkenCombo } from "tekken-parser";
+
+export type InputsType = "1" | "2" | "3" | "4" | "1+2" | "1+3" | "1+4" | "2+3" | "2+4" | "3+4" | "1+2+3+4"
 | "b" | "f" | "u" | "d" | "uf" | "ub" | "df" | "db" | "bhold" | "bracketl" | "bracketr"
 | "dbhold" | "dfhold" | "dhold" | "fhold" | "n" | "ubhold" | "ufhold" | "uhold";
 
 export type PropertiesType = "powercrush" | "tornado" | "heat" | "homing" | "chip";
 
 export type MoveType = {
-    inputs: string[],
+    combo: TekkenCombo,
     hitProperties?: PropertiesType[],
     hint?: string
 }
